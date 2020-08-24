0857a37ccdbc1e24d5157d5a8ddd53a80225987b
callmeberzerker <smarkovski@gmail.com>
2020-08-24 08:47:38 +0200

    backend project setup

---
+++ backend/cmd/library/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+
+	log.Info().Msg("Stuff goes here")
+}
